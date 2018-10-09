12d278551eb951792721c05893f308c2878d5c07
Yehor Lvivski <yehor@lvivski.com>
2018-10-09 11:17:29 +0200

    Rename

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as dotenv from 'dotenv';
-import IgBot from './igbot';
+import Instamator from './instamator';
 
 dotenv.config();
 
-const igbot = new IgBot();
+const instamator = new Instamator();
 
-igbot.login({
+instamator.login({
   password: process.env.PASSWORD,
   username: process.env.USERNAME,
 })
-.then(() => igbot.getUserInfo('lvivski').catch((error) => console.log("error", error)))
+.then(() => instamator.getUserInfo('lvivski'))
 .then((data) => console.log(data))
 .catch((error) => console.log("error", error));
